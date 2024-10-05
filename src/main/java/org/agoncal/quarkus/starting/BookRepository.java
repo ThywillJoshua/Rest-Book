92ca98724360b4d9ba18b768f3d23287f76e58be
Thywill Tonye Joshua <thywilljoshua@gmail.com>
2024-10-05 14:50:06 +0200

    Add tests

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,6 +1,8 @@
 package org.agoncal.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +11,13 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class BookRepository {
-
+    @ConfigProperty(name = "books.genre")
+    private String genre;
 
     public List<Book> getAllBooks() {
         List<Book> books = new ArrayList<>();
 
-        books.add(new Book(1, "George Orwell", "1984", 1949, "Dystopian"));
+        books.add(new Book(1, "George Orwell", "1984", 1949, genre));
         books.add(new Book(2, "J.R.R. Tolkien", "The Hobbit", 1937, "Fantasy"));
         books.add(new Book(3, "Harper Lee", "To Kill a Mockingbird", 1960, "Fiction"));
         books.add(new Book(4, "F. Scott Fitzgerald", "The Great Gatsby", 1925, "Classics"));
